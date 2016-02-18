0efc3cfda47f34ac583e7eea20ee0657e55b5edc
ClamSec <lamberson_christopher@columbusstate.edu>
2016-02-18 11:56:48 -0500

    Add templates for more content, set up views.py for more content.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 from flask import render_template
 from app import app
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"), 404
+
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
 def index():
 	return render_template("index.html", title="Cyber Security Awareness Module")
+
+@app.route("/password-cracking/")
+def passwordCracking():
+	return render_template("password-cracking.html", title="Password Cracking")
+
+@app.route("/password-strength/")
+def passwordStrength():
+	return render_template("password-strength.html", title="Password Strength")
