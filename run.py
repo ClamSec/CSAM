7a934f23be2ccafde01d749a840d616f6843803c
Nathan Gibson <gibson_nathaniel1@columbusstate.edu>
2016-05-16 10:50:08 -0400

    added logging

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 #!flask/bin/python
+
 from app import app
-app.run(debug=True, host='0.0.0.0')
+
+import logging
+logging.basicConfig(filename='error.log',level=logging.DEBUG)
+
+app.run(debug=True, host='0.0.0.0',port=5555)
